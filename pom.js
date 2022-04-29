0e67c4e2435b1cc7d6e49a9f7f2f9cb0b84153fd
KernelDeimos <eric@centos1.rack1.local>
2022-04-29 10:47:08 -0400

    Add foam build pom and gradle config

---
+++ pom.js
@@ -0,0 +1,9 @@
+foam.POM({
+  name: 'tryfoam',
+  version: 1,
+  projects: [
+    { name: 'foam3/src/pom' },
+    { name: 'foam3/src/foam/nanos/pom' },
+    { name: 'foam3/src/foam/support/pom' },
+  ]
+});
